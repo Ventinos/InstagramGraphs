64efe8a4e9d87c8bf1f8a905aec6fbe1692c8915
Pedro Ribeiro <122578432+PedroRibeiroA123@users.noreply.github.com>
2023-10-25 19:37:24 -0300

    Add files via upload

---
+++ preReqs.py
@@ -0,0 +1,57 @@
+import time
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+
+
+def save_credentials(username, password):
+    with open('credentials.txt', 'w') as file:
+        file.write(f"{username}\n{password}")
+
+
+def load_credentials():
+    if not os.path.exists('credentials.txt'):
+        return None
+
+    with open('credentials.txt', 'r') as file:
+        lines = file.readlines()
+        if len(lines) >= 2:
+            return lines[0].strip(), lines[1].strip()
+
+    return None
+
+
+def prompt_credentials():
+    username = input("Enter your Instagram username: ")
+    password = input("Enter your Instagram password: ")
+    save_credentials(username, password)
+    return username, password
+
+
+def login(bot, username, password):
+    bot.get('https://www.instagram.com/accounts/login/')
+    time.sleep(1)
+
+    # Check if cookies need to be accepted
+    try:
+        element = bot.find_element(By.XPATH, "/html/body/div[4]/div/div/div[3]/div[2]/button")
+        element.click()
+    except NoSuchElementException:
+        print("[Info] - Instagram did not require to accept cookies this time.")
+
+    print("[Info] - Logging in...")
+    username_input = WebDriverWait(bot, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+    password_input = WebDriverWait(bot, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+
+    username_input.clear()
+    username_input.send_keys(username)
+    password_input.clear()
+    password_input.send_keys(password)
+
+    login_button = WebDriverWait(bot, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
+    login_button.click()
+    time.sleep(10)
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import time
-import os
+import preReqs
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,137 +7,123 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager as CM
-from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 
-def save_credentials(username, password):
-    with open('credentials.txt', 'w') as file:
-        file.write(f"{username}\n{password}")
 
-
-def load_credentials():
-    if not os.path.exists('credentials.txt'):
-        return None
-
-    with open('credentials.txt', 'r') as file:
-        lines = file.readlines()
-        if len(lines) >= 2:
-            return lines[0].strip(), lines[1].strip()
-
-    return None
-
-
-def prompt_credentials():
-    username = input("Enter your Instagram username: ")
-    password = input("Enter your Instagram password: ")
-    save_credentials(username, password)
-    return username, password
-
-
-def login(bot, username, password):
-    bot.get('https://www.instagram.com/accounts/login/')
-    time.sleep(1)
-
-    # Check if cookies need to be accepted
-    try:
-        element = bot.find_element(By.XPATH, "/html/body/div[4]/div/div/div[3]/div[2]/button")
-        element.click()
-    except NoSuchElementException:
-        print("[Info] - Instagram did not require to accept cookies this time.")
-
-    print("[Info] - Logging in...")
-    username_input = WebDriverWait(bot, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
-    password_input = WebDriverWait(bot, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
-
-    username_input.clear()
-    username_input.send_keys(username)
-    password_input.clear()
-    password_input.send_keys(password)
-
-    login_button = WebDriverWait(bot, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
-    login_button.click()
-    time.sleep(10)
-
-
-def scrape_followers(bot, username, user_input):
+def scrape_followers(bot, username):
     bot.get(f'https://www.instagram.com/{username}/')
     time.sleep(3.5)
 
-    #extracting max followers numbers
-    
-    source=bot.page_source
-    soup = BeautifulSoup(source, 'html.parser')
-    links = soup.find_all('span',class_='_ac2a')
-    for i in range(len(links)):
-        if str(links[i].get('title')).isnumeric():
-            maxFollowers = int(links[i].get('title'))
-    #_aady class
-    #vendo se a conta eh privada:
-    private = soup.find_all('div',class_='_aady')
-    if len(private)>0 :
-        print('foooi')
-
-    WebDriverWait(bot, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, "//a[contains(@href, '/followers')]"))).click()
-    time.sleep(2)
-    print(f"[Info] - Scraping followers for {username}...")
-  
     users = set()
 
-    while len(users) < 300:
+    # vendo se a conta eh privada:
+    source = bot.page_source
+    soup = BeautifulSoup(source, 'html.parser')
+    private = soup.find_all('div', class_='_aady')
+    if len(private) > 0:
+        return users
+
+    WebDriverWait(bot, TIMEOUT).until(
+        EC.presence_of_element_located((By.XPATH, "//a[contains(@href, '/followers')]"))).click()
+    time.sleep(2)
+
+    print(f"Scraping followers for {username}...")
+
+    flag = True
+    count = 0
+
+    while flag:
+        prev = len(users)
         followers = bot.find_elements(By.XPATH, "//a[contains(@href, '/')]")
 
         for i in followers:
             if i.get_attribute('href'):
                 users.add(i.get_attribute('href').split("/")[3])
-                print(len(users),'/{maxFollowers}')
+                print("Total:", len(users))
             else:
                 continue
+
+        if len(users) == prev:
+            count += 1
+
+        flag = count < 6
 
         ActionChains(bot).send_keys(Keys.END).perform()
         time.sleep(1)
 
-   # users = list(users)[:user_input]  # Trim the user list to match the desired number of followers
-
-    print(f"[Info] - Saving followers for {username}...")
-    with open(f'{username}_followers.txt', 'a') as file:
-        file.write('\n'.join(users) + "\n")
-
     return users
 
 
+def scrape_follows(bot, username):
+    bot.get(f'https://www.instagram.com/{username}/')
+    time.sleep(3.5)
+
+    users = set()
+
+    # vendo se a conta eh privada:
+    source = bot.page_source
+    soup = BeautifulSoup(source, 'html.parser')
+    private = soup.find_all('div', class_='_aady')
+    if len(private) > 0:
+        return users
+
+    WebDriverWait(bot, TIMEOUT).until(
+        EC.presence_of_element_located((By.XPATH, "//a[contains(@href, '/following')]"))).click()
+    time.sleep(2)
+
+    print(f"Scraping follows for {username}...")
+
+    flag = True
+    count = 0
+
+    while flag:
+        prev = len(users)
+        followers = bot.find_elements(By.XPATH, "//a[contains(@href, '/')]")
+
+        for i in followers:
+            if i.get_attribute('href'):
+                users.add(i.get_attribute('href').split("/")[3])
+                print("Total:", len(users))
+            else:
+                continue
+
+        if len(users) == prev:
+            count += 1
+
+        flag = count < 6
+
+        ActionChains(bot).send_keys(Keys.END).perform()
+        time.sleep(1)
+
+    return users
+
 def scrape():
-    credentials = load_credentials()
+    credentials = preReqs.load_credentials()
 
     if credentials is None:
-        username, password = prompt_credentials()
+        username, password = preReqs.prompt_credentials()
     else:
         username, password = credentials
 
-    user_input = 1100
-
-    #usernames = ["comppet.ufu", "bateriacomputaria", "aaacompufu"]
-    usernames = ['pedro_ara_ri']
+    usernames = ["comppet.ufu", "bateriacomputaria", "aaacompufu"]
     options = webdriver.ChromeOptions()
     # options.add_argument("--headless")
     options.add_argument('--no-sandbox')
     options.add_argument("--log-level=3")
+
     mobile_emulation = {
         "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/90.0.1025.166 Mobile Safari/535.19"}
     options.add_experimental_option("mobileEmulation", mobile_emulation)
 
     bot = webdriver.Chrome(executable_path=CM().install(), options=options)
 
-    login(bot, username, password)
+    preReqs.login(bot, username, password)
 
     comps = set()
 
     for user in usernames:
         user = user.strip()
-        comps.update(scrape_followers(bot, user, user_input))
-
-    print(f"[Info] - Saving followers for computeiros...")
-    with open(f'computeiros_followers.txt', 'a') as file:
-        file.write('\n'.join(comps) + "\n")
+        comps.update(scrape_followers(bot, user))
 
     bot.quit()
 
