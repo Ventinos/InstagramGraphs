c05ff27b395587bfa7748f404f46c3579b1f7f7a
MariaLuisaGabriel <maria.luisa.gabriel.domingues@ufu.br>
2023-11-24 14:35:41 -0300

    fizemo os options todos

--- main2.py
+++ main2.py
@@ -2,6 +2,7 @@ from src import graphs
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
 def main():
     following = None
     followers = None
@@ -14,10 +15,9 @@ def main():
     G = nx.DiGraph()
     G.add_edges_from(graph)
     G.remove_edges_from(nx.selfloop_edges(G))
-
-    while option != 4:
-        print('Option 1: Print Graph\nOption 2: Page Rank\nOption 3: Hits\nOption 4: Exit')
-        option = int(input('Select an option'))
+    while option != 5:
+        print('Option 1: Print Graph\nOption 2: Page Rank\nOption 3: Hits\nOption 4: Six Degrees of Separation\nOption 5: Exit\n')
+        option = int(input('Select an option: '))
         match option:
             case 1:
                 pos = nx.kamada_kawai_layout(G)
@@ -27,11 +27,47 @@ def main():
                 plt.show()
             case 2:
                 #retorna um dicionario com os ranks
+                pos = nx.kamada_kawai_layout(G)
                 pagerank = nx.pagerank(G)
+
+                #gera um array chamado "cores" que possui os itens de cada chave do dicionario "pagerank", na ordem em que aparece os nos no G.nodes()
+                cores = [pagerank.get(no) for no in G.nodes()]
+                
+                #pinta cada no com sua cor correspondente (correspondencia via equivalencia de posicao entre os arrays "cores" e G.nodes()
+                nx.draw(G, pos, with_labels=True, node_size=100, node_color = cores, font_size=8, font_color='black', 
+                        font_weight='bold', width=1, edge_color='skyblue', cmap=plt.cm.plasma)
+                plt.show()
             case 3:
+                pos = nx.kamada_kawai_layout(G)
                 #retorna uma dupla cm 2 dicionarios com ranks, um dos portais e outro das autoridades
-                hits = nx.hits(G)
+                coresHits = []
+                (hub,autho) = nx.hits(G)
+                for name in G.nodes():
+                    indHub = hub.get(name)
+                    indAutho = autho.get(name)
+                    if indAutho-indHub<0.00 : #caso seja portal
+                        coresHits.append('pink')
+                    else : #caso seja autoridade
+                        coresHits.append('yellow')
+
+                nx.draw(G, pos, with_labels=True, node_size=100, node_color = coresHits, font_size=8, font_color='black', 
+                        font_weight='bold', width=1, edge_color='skyblue')
+                plt.show()
             case 4:
+                totalCaminhos = 0
+                nroCaminhos = 0
+                for i in G.nodes():
+                    for j in G.nodes():
+                        if i!=j :
+                            try:
+                                caminho = nx.shortest_path(G,i,j)
+                                nroCaminhos+=1
+                                totalCaminhos += len(caminho)
+                            except nx.NetworkXNoPath:
+                                continue
+
+                print('media = ' + str(float(totalCaminhos/nroCaminhos)) + '\n')
+            case 5:
                 break
             case _:
                 print('erro')
